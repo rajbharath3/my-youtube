6282a8c5c1688659765a08daa4f6c74fb885b9c2
rajbharath3 <135356999+rajbharath3@users.noreply.github.com>
2023-11-25 15:37:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App,{appRouter} from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
